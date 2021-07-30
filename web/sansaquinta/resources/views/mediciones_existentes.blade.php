718c3f50acfbe5cfe7224534c025e22b5e6391d5
Sebastian Gaete <locksdanny@outlook.com>
2021-07-29 20:27:44 -0400

    layout de mediciones_existentes

--- web/sansaquinta/resources/views/mediciones_existentes.blade.php
+++ web/sansaquinta/resources/views/mediciones_existentes.blade.php
@@ -4,6 +4,21 @@
 <div class="card">
     <div class="card-body m-4">
         <h4 class="card-title">Mediciones Existentes</h4>
+        <br>
+        <table class="table table-hover table-bordered table-striped talbe-responsive">
+            <thead>
+                <tr>
+                    <td>Fecha</td>
+                    <td>Hora</td>
+                    <td>Medidor</td>
+                    <td>Valor</td>
+                    <td>Acciones</td>
+                </tr>
+            </thead>
+            <tbody id="tbody-lecturas">
+
+            </tbody>
+        </table>
     </div>
 </div>
 
