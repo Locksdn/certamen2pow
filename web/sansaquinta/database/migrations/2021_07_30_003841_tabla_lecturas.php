aa561fec5e02f8c619bb05f631c0036b746fe2a3
Sebastian Gaete <locksdanny@outlook.com>
2021-07-29 21:04:41 -0400

    preparando tabla y modelo

---
+++ web/sansaquinta/database/migrations/2021_07_30_003841_tabla_lecturas.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class TablaLecturas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lecturas', function (Blueprint $table) {
+            $table->id();
+
+            $table->date("fecha");
+            $table->string("hora", 5);
+            $table->integer("medidor")->unsigned();
+            $table->string("direccion", 200);
+            $table->integer("valor")->unsigned();
+            $table->string("tipo_medida", 5);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lecturas');
+    }
+}
